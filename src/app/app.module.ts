96cb07940a1c019a15b535ba7c915ba8a97ddcf0
Larissa de Paula <larissadepaulagoncalves@gmail.com>
2023-07-31 12:21:58 -0300

    style/logo fritokas e pagina de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { ClienttableComponent } from './components/clienttable/clienttable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoComponent,
+    ClienttableComponent
   ],
   imports: [
     BrowserModule,
